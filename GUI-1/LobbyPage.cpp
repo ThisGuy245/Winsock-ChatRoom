f72856b123f765fc2390a226ab097a283623de5c
ThisGuy245 <thomasisherwood@outlook.com>
2024-12-11 18:17:38 +0000

    Almost done

--- GUI-1/LobbyPage.cpp
+++ GUI-1/LobbyPage.cpp
@@ -11,10 +11,6 @@
 
 LobbyPage::LobbyPage(int x, int y, int w, int h)
     : Fl_Group(x, y, w, h), chatDisplay(nullptr), chatBuffer(nullptr), inputBox(nullptr) {
-    // Initialize chat data
-    chatData = std::make_shared<ChatData>();
-    chatData->load("chat_history.xml");
-
     // Create UI elements
     Fl_Box* navBar = new Fl_Box(x, y, w, 50, "");
     navBar->box(FL_FLAT_BOX);
@@ -24,109 +20,92 @@ LobbyPage::LobbyPage(int x, int y, int w, int h)
     titleBox->labelsize(18);
     titleBox->align(FL_ALIGN_CENTER);
 
-    connectionStatusBox = new Fl_Box(x + w - 150, y + 10, 140, 30, "Connected");
+    connectionStatusBox = new Fl_Box(x + w - 150, y + 10, 140, 30, "Disconnected");
     connectionStatusBox->labelsize(14);
     connectionStatusBox->align(FL_ALIGN_CENTER);
 
-    // Player sidebar
-    playerSidebar = new Fl_Box(x, y + 50, 200, h - 100, "");
+    playerSidebar = new Fl_Box(x, y + 50, 200, h - 100, "Players:\n");
     playerSidebar->box(FL_UP_BOX);
-    playerSidebar->label("Players\n");
     playerSidebar->align(FL_ALIGN_TOP_LEFT | FL_ALIGN_INSIDE);
 
-    // Chat display
     chatBuffer = new Fl_Text_Buffer();
     chatDisplay = new Fl_Text_Display(x + 220, y + 60, w - 240, h - 150);
     chatDisplay->box(FL_BORDER_BOX);
-    chatDisplay->color(FL_WHITE);
     chatDisplay->buffer(chatBuffer);
 
-    // Input box and send button
     inputBox = new Fl_Input(x + 220, y + h - 90, w - 320, 30, "Message:");
-    inputBox->align(FL_ALIGN_TOP | FL_ALIGN_INSIDE);
+    inputBox->align(FL_ALIGN_TOP);
 
     Fl_Button* sendButton = new Fl_Button(x + w - 100, y + h - 90, 80, 30, "Send");
-    sendButton->callback(send_button_callback, (void*)this);
+    sendButton->callback(send_button_callback, this);
 
-    // Display chat history
-    for (const auto& message : chatData->getMessages()) {
-        addChatMessage(message.user + ": " + message.content);
-    }
-
-    end(); // End the group
+    end(); // End FLTK group
 }
 
 void LobbyPage::setServerSocket(std::shared_ptr<ServerSocket> server, const std::string& username) {
     serverSocket = server;
     localUsername = username;
+    connectionStatusBox->label("Connected as Host");
     addPlayer(localUsername);
-    std::string joinMessage = localUsername + " joined the chat.";
-    broadcastMessage(joinMessage, nullptr);  // Broadcast as a regular client message, no specific sender
+    broadcastMessage(localUsername + " has hosted the chat.", nullptr);
     broadcastUserList();
 }
 
 void LobbyPage::setClientSocket(std::shared_ptr<ClientSocket> client, const std::string& username) {
     clientSocket = client;
     localUsername = username;
-
-    // Notify host of the username
-    clientSocket->send(localUsername + " joined the chat.");
-
-    // Add this client to the player list
+    connectionStatusBox->label("Connected as Client");
     addPlayer(localUsername);
-
-    // Display chat history
-    for (const auto& message : chatData->getMessages()) {
-        addChatMessage(message.user + ": " + message.content);
-    }
-
-    // Handle incoming messages via Update()
+    broadcastMessage(localUsername + " has hosted the chat.", nullptr);
+    broadcastUserList();
 }
 
 void LobbyPage::Update() {
-    // Server-side logic
     if (serverSocket) {
-        // Accept new clients
-        auto newClient = serverSocket->accept();
-        if (newClient) {
-            serverSocket->addClient(newClient);
-            addPlayer(newClient->getUsername()); // Sync username
-            broadcastUserList();
-        }
-
-        // Receive and broadcast messages
-        for (auto& client : serverSocket->getClients()) {
-            std::string message;
-            if (client->receive(message)) {
-                chatData->addMessage(client->getUsername(), message); // Include usernames
-                broadcastMessage(message, client);
-                addChatMessage(client->getUsername() + ": " + message);
-            }
-        }
+        handleServerUpdates();
     }
-    // Client-side logic
     if (clientSocket) {
+        handleClientUpdates();
+    }
+}
+
+void LobbyPage::handleServerUpdates() {
+    auto newClient = serverSocket->accept();
+    if (newClient) {
+        serverSocket->addClient(newClient);
+        broadcastUserList();
+    }
+
+    for (auto& client : serverSocket->getClients()) {
         std::string message;
-        while (clientSocket->receive(message)) {
+        if (client->receive(message)) {
+            broadcastMessage(message, client);
             addChatMessage(message);
         }
+    }
+}
+
+void LobbyPage::handleClientUpdates() {
+    std::string message;
+    while (clientSocket->receive(message)) {
+        addChatMessage(message);
     }
 }
 
 void LobbyPage::broadcastMessage(const std::string& message, std::shared_ptr<ClientSocket> sender) {
     if (serverSocket) {
         for (const auto& client : serverSocket->getClients()) {
-            // Only send messages to clients if they are not the sender.
-            if (!sender || client != sender) {
+            if (client != sender) {
                 client->send(message);
             }
         }
     }
+    //addChatMessage(message); // Display locally
 }
 
 void LobbyPage::broadcastUserList() {
     if (serverSocket) {
-        std::string userList = "Users in the lobby:\n";
+        std::string userList = "Players:\n";
         for (const auto& client : serverSocket->getClients()) {
             userList += client->getUsername() + "\n";
         }
@@ -141,7 +120,7 @@ void LobbyPage::addPlayer(const std::string& playerName) {
 }
 
 void LobbyPage::updatePlayerList() {
-    std::string playerText = "Players\n";
+    std::string playerText = "Players:\n";
     for (const auto& player : players) {
         playerText += player + "\n";
     }
@@ -156,18 +135,19 @@ void LobbyPage::addChatMessage(const std::string& message) {
 
 void LobbyPage::send_button_callback(Fl_Widget* widget, void* userdata) {
     auto* lobbyPage = static_cast<LobbyPage*>(userdata);
-    const char* message = lobbyPage->inputBox->value();
+    const char* text = lobbyPage->inputBox->value();
+    if (text && text[0] != '\0') {
+        std::string message = lobbyPage->localUsername + ": " + text;
+        lobbyPage->addChatMessage(message);
 
-    if (message && message[0] != '\0') {
-        std::string formattedMessage = lobbyPage->localUsername + ": " + message;
-        lobbyPage->chatData->addMessage(lobbyPage->localUsername, message);
-        lobbyPage->addChatMessage(formattedMessage);
-        if (lobbyPage->clientSocket) {
-            lobbyPage->clientSocket->send(formattedMessage);
+        if (lobbyPage->serverSocket) {
+            lobbyPage->broadcastMessage(message, nullptr);
         }
-        else if (lobbyPage->serverSocket) {
-            lobbyPage->broadcastMessage(formattedMessage, nullptr); // Message from host as a client
+        else if (lobbyPage->clientSocket) {
+            lobbyPage->broadcastMessage(message, nullptr);
+            //lobbyPage->clientSocket->send(message);
         }
-        lobbyPage->inputBox->value(""); // Clear input box
+
+        lobbyPage->inputBox->value("");
     }
 }
--- GUI-1/LobbyPage.hpp
+++ GUI-1/LobbyPage.hpp
@@ -3,46 +3,43 @@
 
 #include <FL/Fl_Group.H>
 #include <FL/Fl_Text_Display.H>
-#include <FL/Fl_Box.H>
-#include <FL/Fl_Input.H>
 #include <FL/Fl_Button.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Box.H>
+#include <string>
 #include <vector>
 #include <memory>
-#include <string>
-#include "ChatData.hpp"
 #include "ServerSocket.h"
 #include "ClientSocket.h"
 
 class LobbyPage : public Fl_Group {
-public:
-    LobbyPage(int x, int y, int w, int h);
-
-    void setServerSocket(std::shared_ptr<ServerSocket> server, const std::string& username);
-    void setClientSocket(std::shared_ptr<ClientSocket> client, const std::string& username);
-    void Update();
-
-    std::shared_ptr<ClientSocket> getClientSocket() const { return clientSocket; };
-    void addChatMessage(const std::string& message);
 private:
-    static void send_button_callback(Fl_Widget* widget, void* userdata);
-
-    void broadcastMessage(const std::string& message, std::shared_ptr<ClientSocket> sender);
-    void broadcastUserList();
-    void addPlayer(const std::string& playerName);
-    void updatePlayerList();
-
-    std::shared_ptr<ChatData> chatData;
-    std::shared_ptr<ServerSocket> serverSocket;
-    std::shared_ptr<ClientSocket> clientSocket;
-
-    std::vector<std::string> players;
-    std::string localUsername;
-
     Fl_Text_Display* chatDisplay;
     Fl_Text_Buffer* chatBuffer;
     Fl_Input* inputBox;
     Fl_Box* playerSidebar;
     Fl_Box* connectionStatusBox;
+
+    std::shared_ptr<ServerSocket> serverSocket;
+    std::shared_ptr<ClientSocket> clientSocket;
+
+    std::string localUsername;
+    std::vector<std::string> players;
+
+    static void send_button_callback(Fl_Widget* widget, void* userdata);
+    void handleServerUpdates();
+    void handleClientUpdates();
+    void addPlayer(const std::string& playerName);
+    void updatePlayerList();
+
+public:
+    LobbyPage(int x, int y, int w, int h);
+    void setServerSocket(std::shared_ptr<ServerSocket> server, const std::string& username);
+    void setClientSocket(std::shared_ptr<ClientSocket> client, const std::string& username);
+    void Update();
+    void broadcastMessage(const std::string& message, std::shared_ptr<ClientSocket> sender = nullptr);
+    void broadcastUserList();
+    void addChatMessage(const std::string& message);
 };
 
-#endif
+#endif // LOBBY_PAGE_HPP
